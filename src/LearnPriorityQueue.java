fd2c10dc3c988ee8bb2f65c9fcd9f1424df6d414
rahulkumarhavit <rahulkumarhavit@gmail.com>
2023-05-21 22:04:12 +0530

    priority queue

--- src/LearnPriorityQueue.java
+++ src/LearnPriorityQueue.java
@@ -1,2 +1,22 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class LearnPriorityQueue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.offer(23);
+        pq.offer(325);
+        pq.offer(7);
+
+        System.out.println(pq);
+        System.out.println(pq.peek());
+        pq.poll();
+        System.out.println(pq);
+        System.out.println(pq.peek());
+
+
+
+
+    }
 }
